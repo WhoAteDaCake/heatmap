2e35de7188f968895c1d7413255819a967767b99
WhoAteDaCake <doitsimplefy@gmail.com>
2017-07-03 16:18:57 +0100

    Moved /client to /src

--- client/actions/index.js
+++
@@ -1,7 +0,0 @@
-import * as types from 'config/actionTypes';
-
-export function addLike(dispatch) {
-	return () => {
-		dispatch({ type: types.USER_ADD, likes: 1 });
-	};
-}
--- client/config/actionTypes.js
+++
@@ -1,1 +0,0 @@
-export const USER_ADD = 'USER_ADD';
