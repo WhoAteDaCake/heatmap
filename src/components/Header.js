7ab642c13d6db44aa673572f45cda1f6d674a977
Pedro Mendonca <peumendonca@gmail.com>
2017-07-05 13:39:22 +0100

    Added Header object

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div>Header</div>
+    );
+  }
+}
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,13 +1,16 @@
+// @flow
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import mapRoutes from 'helpers/mapRoutes';
 import routes from 'constants/routes';
+import Header from 'components/Header.js'
 
 export default function App() {
   return (
     <div className="app">
       <div className="header">Header</div>
+      <Header/>
       <div className="app__body">
         <Switch>
           {mapRoutes(routes)}
