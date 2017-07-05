6cfa4ec4057fa1d7bb5e8fcfb815e37bef904525
WhoAteDaCake <doitsimplefy@gmail.com>
2017-07-05 15:57:20 +0100

    Added work around for creating stylesheets

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,14 +1,52 @@
-export default {
-  primary: {
-    dark: '#303F9F',
-    main: '#2196F3',
-    light: '#C5CAE9',
-    text: '#FFFFFF',
-  },
-  accent: '#448AFF',
-  text: {
-    primary: '#212121',
-    secondary: '#757575',
-  },
-  divider: 'rgba(189, 189, 189, 0.35)',
+// @flow
+/* eslint-disable quote-props */
+// $FlowIgnore it's a bug in flow
+import { createMuiTheme } from 'material-ui/styles';
+// $FlowIgnore it's a bug in flow
+import red from 'material-ui/colors/red';
+// $FlowIgnore it's a bug in flow
+import createPalette from 'material-ui/styles/palette';
+
+const primary = {
+  '50': '#e4e5e6',
+  '100': '#babec1',
+  '200': '#8d9397',
+  '300': '#5f676d',
+  '400': '#3c474e',
+  '500': '#1a262f',
+  '600': '#17222a',
+  '700': '#131c23',
+  '800': '#0f171d',
+  '900': '#080d12',
+  A100: '#55aaff',
+  A200: '#2290ff',
+  A400: '#0077ee',
+  A700: '#006ad4',
+  contrastDefaultColor: 'light',
 };
+
+const accent = {
+  '50': '#ffeee9',
+  '100': '#fed5c8',
+  '200': '#feb9a4',
+  '300': '#fe9d7f',
+  '400': '#fd8863',
+  '500': '#fd7348',
+  '600': '#fd6b41',
+  '700': '#fc6038',
+  '800': '#fc5630',
+  '900': '#fc4321',
+  A100: '#ffffff',
+  A200: '#ffffff',
+  A400: '#ffd4cd',
+  A700: '#ffbdb3',
+  contrastDefaultColor: 'dark',
+};
+
+export default createMuiTheme({
+  palette: createPalette({
+    primary, // Purple and green play nicely together.
+    accent,
+    error: red,
+  }),
+});
---
+++ src/helpers/material.js
@@ -0,0 +1,11 @@
+// @flow
+// $FlowIgnore bug in flow where it doesn't allow deeper exports
+import { createStyleSheet as styleSheet, withStyles as wrapStyles } from 'material-ui/styles';
+
+export function createStyleSheet(name: string, fn: Function) {
+  return styleSheet('MyLink', fn);
+}
+
+export function withStyles(sheet: Object) {
+  return (component: ReactClass<*>) => wrapStyles(sheet)(component);
+}
