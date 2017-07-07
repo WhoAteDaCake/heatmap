d09d16cec3fe9eadee4fe8d2a4f8bc450509f4d6
WhoAteDaCake <doitsimplefy@gmail.com>
2017-07-07 11:14:00 +0100

    Should now show correct header title

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,10 +1,17 @@
 import Home from 'routes/Home';
+import Projects from 'routes/Projects';
 
 const routes = [
   {
     path: '/',
     component: Home,
     exact: true,
+    name: 'Home'
+  }, {
+    path: '/projects',
+    component: Projects,
+    exact: true,
+    name: 'Projects'
   },
 ];
 
