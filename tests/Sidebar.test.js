488b9edeab8038cd875dfd89c887629346fdad02
WhoAteDaCake <doitsimplefy@gmail.com>
2017-07-07 14:37:47 +0100

    Added alias for babel

--- tests/Sidebar.test.js
+++ tests/Sidebar.test.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Sidebar from '../src/components/Sidebar.js';
 
 test('Sidebar', () => {
-  const testUnit = new Sidebar();
+  const testUnit = <Sidebar />;
   expect(3).toBe(3);
 });
